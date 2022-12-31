0c90f2e5decd906ae1793f1d37688f318c3c6622
prakaship78 <prakaship78@gmail.com>
2022-12-31 09:45:21 +0530

    add search bar and table view in TopSearches page

--- NetflixClone/Controllers/SearchVC.swift
+++ NetflixClone/Controllers/SearchVC.swift
@@ -8,13 +8,74 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    private var movies : [Movie] = [Movie]()
+    
+    private let searchController : UISearchController = {
+       let controller = UISearchController()
+        controller.searchBar.placeholder = "Search for a Movies and Tv Shows"
+        controller.searchBar.searchBarStyle = .minimal
+        return controller
+    }()
+    
+    private let discoverTable : UITableView = {
+        let table = UITableView()
+        table.register(MovieTableTableViewCell.self, forCellReuseIdentifier: MovieTableTableViewCell.identifier)
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        title = "Top Searches"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        view.addSubview(discoverTable)
+   
+        discoverTable.delegate = self
+        discoverTable.dataSource = self
+        
+        navigationItem.searchController = searchController
+        fetchTopSearches()
     }
     
-
+    private func fetchTopSearches(){
+        ApiCaller.shared.getTopSearchesMovies {[weak self] results in
+            switch results {
+            case.success(let movies):
+                self?.movies = movies
+                DispatchQueue.main.async {
+                    self?.discoverTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        discoverTable.frame = view.bounds
+    }
    
 
+}
+
+extension SearchVC : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieTableTableViewCell.identifier, for: indexPath) as? MovieTableTableViewCell else {return UITableViewCell()}
+        let movie = movies[indexPath.row]
+        cell.configure(with: MovieViewModel(titleName: movie.original_title ?? "No Title", posterUrl: movie.poster_path ?? ""))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    
 }
--- NetflixClone/Managers/ApiCaller.swift
+++ NetflixClone/Managers/ApiCaller.swift
@@ -97,4 +97,20 @@ class ApiCaller {
         }
         task.resume()
     }
+    
+    func getTopSearchesMovies(completion : @escaping (Result<[Movie], Error>) -> Void){
+        guard let url = URL(string: "\(Constants.baseURL)/3/discover/movie?api_key=\(Constants.API_KEY)&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate") else {return}
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data , error == nil else{return}
+            
+            do {
+                let results = try JSONDecoder().decode(MoviesModel.self, from: data)
+                completion(.success(results.results))
+                
+            } catch {
+                completion(.failure(APIError.failedTogetData))
+            }
+        }
+        task.resume()
+    }
 }

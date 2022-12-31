ed36df1616643a93cb1c7546a2829f7530cef714
prakaship78 <prakaship78@gmail.com>
2023-01-01 00:33:45 +0530

    add no downloads text in center

--- NetflixClone/Controllers/DownloadsVC.swift
+++ NetflixClone/Controllers/DownloadsVC.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class DownloadsVC: UIViewController {
+    
+    private let textLabel : UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.text = "No Downloads"
+        label.textAlignment = .center
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        title = "Downloads"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        view.addSubview(textLabel)
+        applyConstraints()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        textLabel.frame = view.bounds
+    }
+    
+  private func  applyConstraints(){
+        let textLabelConstraints = [
+            textLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+        ]
+      NSLayoutConstraint.activate(textLabelConstraints)
     }
     
 
